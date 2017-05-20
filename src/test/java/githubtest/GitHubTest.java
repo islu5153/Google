fb903d425a7d267731852c73a6d80e14bc177e59
islu513 <mohamed7isma7@gmail.com>
2017-05-20 12:24:21 +0530

    first source commit

---
+++ src/test/java/githubtest/GitHubTest.java
@@ -0,0 +1,20 @@
+package githubtest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GitHubTest {
+	public static void main(String[] args) throws IOException {
+	WebDriver wd = new FirefoxDriver();
+	wd.manage().window().maximize();
+	wd.get("https://www.flipkart.com/");
+	File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(src, new File("E:\\VMETRY PROJECTS\\GitHubTest\\screenshots\\flip.jpg"));
+}
+}
